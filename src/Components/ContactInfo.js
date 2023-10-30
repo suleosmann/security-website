30d749ecec62a7c04ce94b810641740646fddb9a
Suleiman <suleosman73@gmail.com>
2023-10-30 19:55:28 +0300

    Made Services Section Responsive

--- src/Components/ContactInfo.js
+++ src/Components/ContactInfo.js
@@ -6,13 +6,13 @@ function ContactInfo() {
       <div className="contact-item">
         <div className="left-info">
           <div className="info-label">CALL US</div>
-          <div className="info-value">+2547123456789</div>
+          <div className="info-value">+254722311294</div>
         </div>
       </div>
       <div className="contact-item">
         <div className="right-info">
           <div className="info-label">EMAIL</div>
-          <div className="info-value">email@email.co.ke</div>
+          <div className="info-value">contact@broomsvillesurveillance.co.ke</div>
         </div>
       </div>
     </div>
