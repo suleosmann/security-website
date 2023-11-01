d4d530f48a98571b940bca70b348e84d1ca1a93c
Suleiman <suleosman73@gmail.com>
2023-11-01 09:53:16 +0300

    Fixed the bug on Navbar mobile

--- src/Components/Navbar.js
+++ src/Components/Navbar.js
@@ -1,31 +1,54 @@
-import React from "react";
+import React, { useRef, useState, useEffect } from "react";
 import "./css/Navbar.css";
 import ScrollTo from "./ScrollTo";
 
 function Navbar() {
+  const menuCheckboxRef = useRef(null);
+  const [isMobile, setIsMobile] = useState(false);
+
+  useEffect(() => {
+    // Check if the screen width is less than 920px to determine if it's mobile
+    const handleResize = () => {
+      setIsMobile(window.innerWidth <= 920);
+    };
+
+    handleResize(); // Initial check
+    window.addEventListener("resize", handleResize);
+
+    return () => {
+      window.removeEventListener("resize", handleResize);
+    };
+  }, []);
+
+  const closeMenu = () => {
+    if (menuCheckboxRef.current && menuCheckboxRef.current.checked) {
+      menuCheckboxRef.current.checked = false;
+    }
+  };
+
   return (
     <div>
       <nav>
         <div className="logo">
           <img src="./images/logoicon.png" alt="Logo" />
         </div>
-        <input type="checkbox" id="click" />
+        {isMobile && (
+          <input type="checkbox" id="click" ref={menuCheckboxRef} />
+        )}
         <label htmlFor="click" className="menu-btn">
           <i className="fas fa-bars"></i>
         </label>
-        <ul>
-          <li>
-            <a href="#">
-              Home
-            </a>
+        <ul className={isMobile && menuCheckboxRef.current?.checked ? "open" : ""}>
+          <li onClick={closeMenu}>
+            <a href="#">Home</a>
           </li>
-          <li>
+          <li onClick={closeMenu}>
             <ScrollTo target="#about-us-section">About</ScrollTo>
           </li>
-          <li>
+          <li onClick={closeMenu}>
             <ScrollTo target="#services-section">Services</ScrollTo>
           </li>
-          <li>
+          <li onClick={closeMenu}>
             <ScrollTo target="#contact-section"><a className="active">Contact</a></ScrollTo>
           </li>
         </ul>
